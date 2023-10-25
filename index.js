9619e7ecb94ca37201e18cfa359400b107da85e9
JRW <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1@github.com>
2023-10-25 14:17:35 +0700

    manapulating text

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
-var h1 = $("h1"); //selecting element
+// selecting element
+var h1 = $("h1");
+var button = $("button");
 
 // manapulating style
 h1.addClass("big-title margin-50");
 // h1.css("font-size", "5rem");
 // h1.css("color", "red");
+
+// manapulating text
+h1.text("Bye");
+button.text("Don't Click Me.")
+button.html("<em>Hey</em>")
 
 
